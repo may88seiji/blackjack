5d072bfdb53eb4fa779476acfb9da0619d5192b7
may88seiji <may88seiji@gmail.com>
2018-11-22 02:40:25 +0900

    add shuffle

--- src/deck.js
+++ src/deck.js
@@ -1,9 +1,9 @@
+let deck = []
 
 export default class Deck{
   constructor() {
   }
   get(){
-    let deck = []
     let card = ''
     let suits = ['spades', 'diamonds', 'clubs', 'hearts']
     let values = ['A','2','3','4','5','6','7','8','9','10','J',"Q",'K']
@@ -17,14 +17,14 @@ export default class Deck{
       })
     })
 
-    // for (let i = 0; i < 10; ){
-    //   let shuffle_01 = Math.floor(Math.random() * deck.length)
-    //   let shuffle_02 = Math.floor(Math.random() * deck.length)
-    //   let tmp = deck[shuffle_01]
+    for (let i = 0; i < 1000; i++){
+      let shuffle_01 = Math.floor(Math.random() * deck.length)
+      let shuffle_02 = Math.floor(Math.random() * deck.length)
+      let tmp = deck[shuffle_01]
 
-    //   deck[shuffle_01] = deck[shuffle_02]
-    //   deck[shuffle_02] = tmp
-    // }
+      deck[shuffle_01] = deck[shuffle_02]
+      deck[shuffle_02] = tmp
+    }
     console.log(deck)
     return deck
   }
