901ebf16c35c978188315b52a2181311a2cd8839
may88seiji <may88seiji@gmail.com>
2018-11-23 16:42:22 +0900

    add judge

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # blackjack
 
-> A Vue.js project
+```
+- 初期カードは52枚。引く際にカードの重複は無いようにする
+- プレイヤーとディーラーの2人対戦。プレイヤーは実行者、ディーラーは自動的に実行
+- 実行開始時、プレイヤーとディーラーはそれぞれ、カードを2枚引く。引いたカードは画面に表示する。ただし、ディーラーの2枚目のカードは分からないようにする
+- その後、先にプレイヤーがカードを引く。プレイヤーが21を超えていたらバースト、その時点でゲーム終了
+- プレイヤーは、カードを引くたびに、次のカードを引くか選択できる
+- プレイヤーが引き終えたら、その後ディーラーは、自分の手札が17以上になるまで引き続ける
+- プレイヤーとディーラーが引き終えたら勝負。より21に近い方の勝ち
+- JとQとKは10として扱う
+- Aはとりあえず「1」としてだけ扱う。「11」にはしない
+- ダブルダウンなし、スプリットなし、サレンダーなし、その他特殊そうなルールなし
+```
 
 ## Build Setup
 
@@ -13,6 +24,9 @@ npm run dev
 
 # build for production with minification
 npm run build
+
+# use sass
+npm install sass-loader node-sass --save-dev
 ```
 
 For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
     <h1>Vue BlackJack</h1>
+    <div>{{ status }}</div>
     <div class="container">
       <button>Restart</button>
-      <button>Hit</button>
-      <button>Stay</button>
+      <button @click="hit">Hit</button>
+      <button @click="stay">Stay</button>
     </div>
-    <div>{{ addedDeck.length }}</div>
-    <div class="flex">
-      <div v-for="(player, index) in players" :key="index" >
+    <div>Deck</div>
+    <div class="deck">{{ addedDeck.length }}</div>
+    <div class="flex players" v-bind:class="{ isJudge: isJudge }">
+      <div v-for="(player, index) in players" :key="index" :class="['player_' + index]">
         <div>{{player.Name}}</div>
-        <div>{{player.Points}}</div>
-        <div v-for="(Hand, index) in player.Hands" :key='index' class='card'>
-          <div>{{Hand.Suit}}</div>
-          <div>{{Hand.Value}}</div>
+        <div class="total">Total {{player.Points}}</div>
+        <div class="cards">
+          <div v-for="(Hand, index) in player.Hands" :key='index' class='card'>
+            <div>{{Hand.Suit}}</div>
+            <div>{{Hand.Value}}</div>
+          </div>
         </div>
       </div>
     </div>
@@ -33,12 +37,17 @@ export default {
       addedDeck: null,
       cardHand: null,
       players: [],
-      currentPlayer: '',
-      points: 0
+      currentPlayer: 0,
+      points: 0,
+      counter: 0,
+      newCard: null,
+      status: 'Now Player_0',
+      isJudge: false
     }
   },
   watch:{
-    players: 'updatePoint'
+    players: 'updatePoint',
+    newCard: ['updatePoint' , 'check']
   },
   mounted: function() {
     this.initDeck(),
@@ -73,33 +82,48 @@ export default {
         })
         e.Points = point
       })
-      console.log(this.players[0].Points)
     },
-    hitMe: function() {
-      var card = deck.pop();
-      players[currentPlayer].Hand.push(card);
-      renderCard(card, currentPlayer);
-      updatePoints();
-      updateDeck();
-      check();
+    hit: function(event) {
+      this.newCard = 0
+      this.newCard = this.addedDeck.pop()
+      this.players[this.currentPlayer].Hands.push(this.newCard)
+    },
+    check: function(event) {
+      if(this.players[this.currentPlayer].Points > 21) {
+        this.status = `Player_${this.players[this.currentPlayer].ID} Lose`
+      }
     },
     stay: function() {
-      // move on to next player, if any
-      if (currentPlayer != players.length-1) {
-        document.getElementById('player_' + currentPlayer).classList.remove('active');
-        currentPlayer += 1;
-        document.getElementById('player_' + currentPlayer).classList.add('active');
+      this.currentPlayer =+ 1
+      this.status = `Now Player_${this.players[this.currentPlayer].ID}`
+      this.playCom()
+    },
+    playCom: function() {
+      while(this.players[this.currentPlayer].Points < 18) {
+        this.hit()
+        this.updatePoint()
+        if(this.players[this.currentPlayer].Points >= 17) {
+          this.judge()
+          break
+        }
       }
-
-      else {
-        end();
+      this.judge()
+    },
+    judge: function() {
+      this.isJudge = true
+      if(this.players[0].Points == this.players[1].Points){
+        this.status = 'Even'
+      }else if(this.players[0].Points > this.players[1].Points){
+        this.status = 'You Win'
+      }else{
+        this.status = 'You Lose'
       }
     }
   }
 }
 </script>
 
-<style>
+<style lang='scss'>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -120,10 +144,41 @@ h1, h2 {
   margin: 0 auto;
 }
 
+.deck {
+  border: 1px solid;
+  display: inline-block;
+  padding: 5px;
+  margin-top: 5px;
+  border-radius: 5px;
+}
+
 .card {
   border: 1px solid;
   border-radius: 5px;
   margin-bottom: 10px;
+  height: 70px;
+}
+
+.player_1 {
+  .total{
+    opacity: 0;
+  }
+  .card {
+    &:not(:first-child) {
+      background-color: black;
+    }
+  }
+}
+
+.isJudge .player_1 {
+    .total{
+    opacity: 1;
+  }
+  .card {
+    &:not(:first-child) {
+      background-color: inherit;
+    }
+  }
 }
 
 </style>
